2e2197ac23e7970e7192919166b8354d34b4c049
Jelle-Degen <149798124+Jelle-Degen@users.noreply.github.com>
2025-08-01 11:11:25 +0200

    trying to get it to run

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     url="https://github.com/Jelle-Degen/Digital-twin",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
